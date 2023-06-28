97255419a494530149f654e6bc3602ed4c45d263
Artem Kopyl <who@Artems-MacBook-Pro.local>
2023-06-28 23:06:02 +0200

    crossover implementation

--- Consts.cs
+++ Consts.cs
@@ -1,6 +1,7 @@
 namespace GA_Solver.Constants{
     class GlobalVars{
-        public static int POPULATION_COUNT = 100;
-        public static int GENERATIONS_COUNT = 20;
+        public static int POPULATION_COUNT = 200;
+        public static int GENERATIONS_COUNT = 1000;
+        public static double MUTATION_CHANCE = 0.05;
     }
 }
--- Program.cs
+++ Program.cs
@@ -32,22 +32,45 @@ namespace GA_Solver{
             List<Chromosome> gen_remainder = Helper.exclude(generation, chrom_a);
             Chromosome chrom_b = gen_remainder[rnd.Next(GlobalVars.POPULATION_COUNT - 1)];
 
-            return chrom_a.get_fitness() > chrom_b.get_fitness() ? chrom_b : chrom_a;
+            return chrom_a.get_fitness() > chrom_b.get_fitness() ? chrom_a : chrom_b;
+        }
+
+        private static Chromosome crossover(Chromosome chrom_a, Chromosome chrom_b){
+            int crossover_point = rnd.Next(1, GlobalVars.POPULATION_COUNT - 1);
+            List<int> offspring_seq = chrom_a.perm.Take(crossover_point).ToList();
+            HashSet<int> appeared = new HashSet<int>(offspring_seq);
+
+            foreach(int gene in chrom_b.perm){
+                if(!appeared.Contains(gene)) offspring_seq.Add(gene);
+            }
+
+            return new Chromosome(offspring_seq);
         }
 
         static void breed(){
             List<Chromosome> next_gen = new List<Chromosome>();
 
             for(int i = 0; i < GlobalVars.POPULATION_COUNT; i++){
+                // SELECTION
                 Chromosome father = tournament_selection(current_generation);
                 List<Chromosome> gen_remainder = Helper.exclude(current_generation, father);
                 Chromosome mother = tournament_selection(gen_remainder);
 
-                // Crossover
-                // Mutation
+                // CROSSOVER
+                Chromosome offspring_a = crossover(father, mother);
+                Chromosome offspring_b = crossover(mother, father);
+
+                next_gen.Add(offspring_a);
+                next_gen.Add(offspring_b);
+
+                // MUTATION
             }
 
-            current_generation = next_gen;
+            current_generation.AddRange(next_gen);
+            current_generation = current_generation
+                .OrderBy(c => c.get_fitness())
+                .Take(GlobalVars.POPULATION_COUNT)
+                .ToList();
         }
 
         public static void Main(){
@@ -57,8 +80,12 @@ namespace GA_Solver{
             }
 
             current_generation = spawn_generation();
-            for(int i = 0; i < GlobalVars.GENERATIONS_COUNT; i++){
+
+            int count = 1;
+            while(true){
                 breed();
+                System.Console.WriteLine($"GENERATION #{count} - {get_best_chromosome(current_generation).get_fitness()}");
+                count += 1;
             }
         }
     }
