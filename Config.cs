cf2a712fd853f078b960c8885c64be437daff2d2
kopyla <stormy_131@proton.me>
2023-08-29 11:04:59 +0200

    added test data' '

--- Config.cs
+++ Config.cs
@@ -1,9 +1,9 @@
 namespace GA_Solver.Consts{
     class Config{
         public static int POPULATION_COUNT = 150;
-        public static int GENERATIONS_COUNT = 100;
+        public static int GENERATIONS_COUNT = 500;
         public static double MUTATION_CHANCE = 0.05;
-        public static int NO_IMPROVEMENT_GEN = 20;
+        public static int NO_IMPROVEMENT_GEN = 100;
         public static int MAP_WIDTH = 4000;
         public static int MAP_HEIGHT = 2200;
     }
